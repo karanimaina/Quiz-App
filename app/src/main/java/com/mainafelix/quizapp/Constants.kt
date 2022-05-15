b47a027a6bfffd598d1b57b028aa327d063f46e5
felixmaina <karani.maina2010@gmail.com>
2022-05-16 02:11:53 +0300

    add a select option xml to edit button color

--- app/src/main/java/com/mainafelix/quizapp/Constants.kt
+++ app/src/main/java/com/mainafelix/quizapp/Constants.kt
@@ -4,8 +4,15 @@ object  Constants{
     fun getQuestion():ArrayList<Question>{
         // create an array of questions
         val  questionsList = ArrayList<Question>()
-        val que1 = Question(1,"what country does this flag belong to?",R.drawable.belgium,"Argentima","Belgium","mauritius","Kenya",1)
+        val que1 = Question(1,"what country does this flag belong to?",R.drawable.belgium,"Argentima","Belgium","mauritius","Kenya",2)
+        val que2 = Question(2,"what country does this flag belong to?",R.drawable.bangladesh,"bangladesh","Nigeria","uganda","tanzania",1)
+        val que3= Question(3,"what country does this flag belong to?",R.drawable.zimbambwe,"Zambia","Usa","belgium","zibambwe",4)
+        val que4 = Question(4,"what country does this flag belong to?",R.drawable.belgium,"Argentima","Belgium","mauritius","Kenya",1)
         questionsList.add(que1)
+        questionsList.add(que2)
+        questionsList.add(que3)
+        questionsList.add(que4)
+
         return  questionsList
     }
 }
--- app/src/main/java/com/mainafelix/quizapp/QuizUiQuestionActivity.kt
+++ app/src/main/java/com/mainafelix/quizapp/QuizUiQuestionActivity.kt
@@ -3,7 +3,9 @@ package com.mainafelix.quizapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
 import android.widget.ProgressBar
+import android.widget.TextView
 
 class QuizUiQuestionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,9 +14,32 @@ class QuizUiQuestionActivity : AppCompatActivity() {
         val  questionList = Constants.getQuestion()
         Log.i("Question size","${questionList.size}")
         val currentPosition = 2
+        val tvProgress  = findViewById<TextView>(R.id.tv_progress)
+        val tvOptionOne  = findViewById<TextView>(R.id.tv_optionOne)
+        val tvOptionTwo  = findViewById<TextView>(R.id.tv_optionTwo)
+        val tvOptionThree  = findViewById<TextView>(R.id.tv_optionThree)
+        val tvOptionFor  = findViewById<TextView>(R.id.tv_optionFour)
+
+        val image  = findViewById<ImageView>(R.id.IvImage)
+        val tvQuestion  = findViewById<TextView>(R.id.tv_wuestion)
+
         val question:Question?= questionList[currentPosition-1]
         val progressBar  = findViewById<ProgressBar>(R.id.progress_bar)
 
         progressBar.progress = currentPosition
+        //set the progressbar equivalent to the textview
+        tvProgress.text = "$currentPosition"+"/"+ progressBar.max
+        //set the Question
+        tvQuestion.text =   question!!.question
+        //et the image resource to the question
+        image.setImageResource(question.image)
+        tvOptionOne.text= question.optionOne
+        tvOptionOne.text= question.optionTwo
+        tvOptionOne.text= question.optionThree
+        tvOptionOne.text= question.optionFour
+        tvOptionOne.text= question.optionOne
+
+
+
     }
 }

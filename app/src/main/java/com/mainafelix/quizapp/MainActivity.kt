1d8477ffd884a5d86eba518c47169c312fa7e068
felixmaina <karani.maina2010@gmail.com>
2022-05-15 20:58:35 +0300

    assign an id to the edit text

--- app/src/main/java/com/mainafelix/quizapp/MainActivity.kt
+++ app/src/main/java/com/mainafelix/quizapp/MainActivity.kt
@@ -3,6 +3,8 @@ package com.mainafelix.quizapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,5 +12,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         //code for removing the system ui
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        val btn = findViewById<Button>(R.id.btnStart)
+        val editTextName = findViewById<EditText>(R.id.etName)
+
+        btn.setOnClickListener {
+            if (editTextName.toString().isEmpty()){
+        }
+
+
     }
 }

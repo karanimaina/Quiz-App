5193fa2ab8cd2f50b43ad7553f0bc67579d34e3f
felixmaina <karani.maina2010@gmail.com>
2022-05-16 01:39:43 +0300

    set the progresbar  progress to the cueent psition

--- app/src/main/java/com/mainafelix/quizapp/QuizUiQuestionActivity.kt
+++ app/src/main/java/com/mainafelix/quizapp/QuizUiQuestionActivity.kt
@@ -3,6 +3,7 @@ package com.mainafelix.quizapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ProgressBar
 
 class QuizUiQuestionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,6 +11,10 @@ class QuizUiQuestionActivity : AppCompatActivity() {
         setContentView(R.layout.activity_quiz_ui_question)
         val  questionList = Constants.getQuestion()
         Log.i("Question size","${questionList.size}")
+        val currentPosition = 2
+        val question:Question?= questionList[currentPosition-1]
+        val progressBar  = findViewById<ProgressBar>(R.id.progress_bar)
 
+        progressBar.progress = currentPosition
     }
 }

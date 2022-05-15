a533868a8c03011d3c92bb735f01097a76e0ff59
felixmaina <karani.maina2010@gmail.com>
2022-05-15 20:54:23 +0300

    add aquizuiquestionsActivity

--- app/src/main/java/com/mainafelix/quizapp/MainActivity.kt
+++ app/src/main/java/com/mainafelix/quizapp/MainActivity.kt
@@ -8,6 +8,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //code for removing the system ui
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
     }
 }
---
+++ app/src/main/java/com/mainafelix/quizapp/QuizUiQuestionActivity.kt
@@ -0,0 +1,11 @@
+package com.mainafelix.quizapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class QuizUiQuestionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz_ui_question)
+    }
+}

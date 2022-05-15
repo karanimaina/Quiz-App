3478fd7217f40ebba99d2f6ff0c2a954bcb7543f
felixmaina <karani.maina2010@gmail.com>
2022-05-15 23:48:08 +0300

    get the total number of questions

--- app/src/main/java/com/mainafelix/quizapp/QuizUiQuestionActivity.kt
+++ app/src/main/java/com/mainafelix/quizapp/QuizUiQuestionActivity.kt
@@ -2,11 +2,14 @@ package com.mainafelix.quizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class QuizUiQuestionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_ui_question)
         val  questionList = Constants.getQuestion()
+        Log.i("Question size","${questionList.size}")
+
     }
 }

8db434648d24443c2aa1d37d29361479c42c9551
felixmaina <karani.maina2010@gmail.com>
2022-05-15 23:35:03 +0300

    add country flags

--- app/src/main/java/com/mainafelix/quizapp/Constants.kt
+++ app/src/main/java/com/mainafelix/quizapp/Constants.kt
@@ -1,4 +1,9 @@
 package com.mainafelix.quizapp
 object  Constants{
     //create one constants which will contain all the questions"
+    fun getQuestion():ArrayList<Question>{
+        // create an array of questions
+        val  questionsList = ArrayList<Question>()
+        val que1 = Question(1,"what country does this flag belong to?",R.drawable.)
+    }
 }
